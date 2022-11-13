50084867d88ee33e3858f1fcfd75fc054126da82
MarieMueller <m.mueller470718@gmail.com>
2022-11-13 12:35:42 -0600

    first commit

--- designs.js
+++ designs.js
@@ -1,10 +1,30 @@
-// Select color input
+
 // Select size input
+const height = document.getElementById('inputHeight');
+const width = document.getElementById('inputWidth');
+const table = document.getElementById('pixelCanvas');
+makeGrid(height, width);
 
 // When size is submitted by the user, call makeGrid()
+document.querySelector('form').addEventListener('submit', function(event){
+    event.preventDefault();
+    table.firstChild.remove();
+    makeGrid(height, width);     
+})
 
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(height, width) {
+    // Your code goes here! 
+    const colorPicker = document.getElementById('colorPicker');
+    for(let i = 0; i < height.value; i++){
+        let row = table.insertRow(i);
+        for(let j = 0; j < width.value; j++){
+            let cell = row.insertCell(j); 
+            // Color input when user click a cell
+            cell.addEventListener("click", function(event){
+                var clickCell = event.target;
+                clickCell.style.backgroundColor = colorPicker.value;
+            })
+                             
+        }
+    }
 }
